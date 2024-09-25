26a80cc4744edb5c52e0b57861d4d7fb49a1cf22
Andrey Aksenov <duuusha@gmail.com>
2024-09-25 10:31:12 +0300

    Insert more data

---
+++ data/insert_data_book.sql
@@ -0,0 +1,11 @@
+INSERT INTO book (id, title, year, author_id)
+VALUES (1, 'War and Peace', 1869, 1),
+       (2, 'Anna Karenina', 1878, 1),
+       (3, 'The Death of Ivan Ilyich', 1886, 1),
+       (4, 'Hadji Murad', 1912, 1),
+       (5, 'Crime and Punishment', 1866, 2),
+       (6, 'The Idiot', 1868, 2),
+       (7, 'The Brothers Karamazov', 1879, 2),
+       (8, 'Eugene Onegin', 1833, 3),
+       (9, 'Boris Godunov', 1831, 3),
+       (10, 'Ruslan and Ludmila', 1820, 3);
